ef69b27e853fc6434fc1f803437a8ecf55e0ceab
ArthurMesan <arthurmesquitasan@gmail.com>
2025-11-12 15:15:03 -0300

    Correção-ALU

--- MIPS32_processor_tests/ALU.vhd
+++ MIPS32_processor_tests/ALU.vhd
@@ -1,5 +1,6 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
 entity ALU is
     Port ( A : in STD_LOGIC_VECTOR (31 downto 0);
@@ -8,17 +9,33 @@ entity ALU is
            Result : out STD_LOGIC_VECTOR (31 downto 0);
            Zero : out STD_LOGIC);
 end ALU;
+
 architecture Behavioral of ALU is
 begin
     process(A, B, ALUControl)
+        variable temp_result : STD_LOGIC_VECTOR(31 downto 0) := (others => '0');
     begin
+
         case ALUControl is
-            when "0000" => Result <= A + B;         -- Addition
-            when "0001" => Result <= A - B;         -- Subtraction
-            when "0010" => Result <= A and B;       -- AND
-            when "0011" => Result <= A or B;        -- OR
-            when others => Result <= (others => '0');
+            when "0000" => -- Addition
+                temp_result := std_logic_vector(unsigned(A) + unsigned(B));
+            when "0001" => -- Subtraction
+                temp_result := std_logic_vector(unsigned(A) - unsigned(B));
+            when "0010" => -- AND
+                temp_result := A and B;
+            when "0011" => -- OR
+                temp_result := A or B;
+            when others =>
+                temp_result := (others => '0');
         end case;
-        Zero <= '1' when Result = "00000000000000000000000000000000" else '0';
+
+        if temp_result = x"00000000" then
+            Zero <= '1';
+        else
+            Zero <= '0';
+        end if;
+
+        Result <= temp_result;
+
     end process;
 end Behavioral;
